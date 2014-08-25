97515990045187b84d510faa2124542a1dc70d00
Georgi <Micev>
2014-08-26 02:10:46 +0300

    todo...

--- new_theme.php
+++ new_theme.php
@@ -1,12 +1,20 @@
-<form method="post">
-    Theme:<input type="text" name="theme">
-    Theme info:<input type="text" name="info">
-    Message title:<input type="text" name="title">
-    Question:<textarea name="message"></textarea>
-    <input type="submit">
+<?php
+require("header.php");
+if(isset($_POST['submit'])){
+    $theme=$_POST['theme'];
+    $info=$_POST['info'];
+    $title=$_POST['title'];
+    $msg=$_POST['message'];
+}
+?>
+<form method="post" class="answerForm">
+    <label for="theme">Theme:</label><input type="text" name="theme" id="theme" required="">
+    <label for="themeInfo">Theme info:</label><input type="text" name="info" id="themeInfo" required="">
+    <label for="themeMessage">Message title:</label><input type="text" name="title" id="themeMessage" required="">
+    <label for="themeQuestion">Question:</label><textarea name="message" id="themeQuestion" required=""></textarea>
+<!--   TODO... here make onclick event to send as in right page-->
+    <input type="submit" name="submit" onclick="" >
 </form>
 <?php
-$theme=$_POST['theme'];
-$info=$_POST['info'];
-$title=$_POST['title'];
-$msg=$_POST['message'];
+require("footer.php");
+?>
