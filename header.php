bcbc14487c529edb91d2cbdefa278e5726cd5638
Georgi <Micev>
2014-08-26 02:13:47 +0300

    put <div id="wrapper">

--- header.php
+++ header.php
@@ -3,20 +3,22 @@
 <head>
     <title>Draco Forum</title>
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/registerAndLog.css">
     <meta charset="utf-8">
 </head>
 <body>
-<img src="images/logo.png">
-<div id="menu">
-   <?php
-   session_start();
-   $log= $_SESSION['isLogged'];
-   if($log){
-       echo 'Hello '.$_SESSION['user'].' you are logged in.'.'<a href="logout.php">Logout</a>';
-   }
-   else{
-       echo 'Hello Guest. Please <a href="login.html">Login</a> or <a href="Register.html">Register</a>.';
-   }
-   ?>
-</div>
-
+<div id="wrapper">
+    <header>
+        <img src="images/logo.png">
+        <div id="menu">
+           <?php
+           session_start();
+           if(isset($_SESSION['isLogged'])){
+               echo 'Hello '.$_SESSION['user'].' you are logged in.'.'<a href="logout.php">Logout</a>';
+           }
+           else{
+               echo 'Hello Guest. Please <a href="login.php">Login</a> or <a href="register.php">Register</a>.';
+           }
+           ?>
+        </div>
+    </header>
